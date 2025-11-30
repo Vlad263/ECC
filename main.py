8b69c4c57153cf1a6e290dfad344dc4b848c7b01
mandoor <168180299+Vlad263@users.noreply.github.com>
2025-11-30 06:54:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 from core.config import load_env
+from pipelines.example_pipeline import run_example
 
 def main():
     print("\n🔷 ECC LAUNCHING 🔷")
     load_env()
+    run_example()
     print("System online. Agents will be attached here.\n")
 
 if __name__ == "__main__":
